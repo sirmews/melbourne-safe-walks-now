44cbff5807ddd1c629aba4ac69dacf45c65835b3
Navishkar Rao <sirmews@gmail.com>
2025-06-21 10:57:50 +1000

    add background color

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -96,7 +96,7 @@ const Index = () => {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <Header />
       
       <MainLayout
