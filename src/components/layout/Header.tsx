5c4b038ad042e64e1ab4c3e29a5a5843c2fc7d33
Elise Young <eliseyoung@Elises-MacBook-Pro.local>
2025-08-22 21:21:12 +0800

    dark mode fix attempt

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,10 +8,9 @@ export const Header = () => {
         <div className="flex justify-between items-center h-16 w-full ml-8">
           <div className="flex items-center gap-2">
             <div className="relative">
-              {/* <Route className="h-8 w-8 text-primary" /> */}
-              <Footprints className="h-8 w-8 text-primary" />
+              <Footprints className="h-8 w-8" />
             </div>
-            <h1 className="text-2xl font-bold text-gray-900 dark:text-white font-playfair">Safer Path</h1>
+            <h1 className="text-2xl font-bold text-gray-900 font-playfair">Safer Path</h1>
           </div>
           
           {/* <div className="flex items-center gap-4">
